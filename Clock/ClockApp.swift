83e9cf5d173bfd25cc95b96c2c39278fa1657897
Russell Gordon <rgordon@lcs.on.ca>
2024-11-08 06:54:01 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Russell Gordon on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
